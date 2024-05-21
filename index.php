1391cd6f7bff88acf1de365653564a3863124f77
Elias Alban <Alban001@users.noreply.github.com>
2024-05-21 14:19:13 -0400

    resposividad y clases agregada a login, agregado breakpoint tables y small

--- index.php
+++ index.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="styles/busqueda.css"></link>
+    <link href="styles/styles.css"></link>
     <title>Login</title>
     <style>
         body{
             background-color: rgba(39, 109, 136, 0.93);
         }
-        .container{
-            margin-top: 150px;
-            padding-left:32%;
+        .container-login{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top:10%;
         }
         input{
             max-width: 300px;
@@ -21,7 +23,7 @@
     </style>
 </head>
 <body>
-    <div class="container">
+    <div class="container-login">
             <div class="row justify-content-center">
                 <form action="login.php" method="POST">
                             <input type="text" name="username" class="form-control" placeholder="Ingresa un nombre"><br>
